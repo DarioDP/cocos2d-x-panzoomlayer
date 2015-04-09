b793b7dc5c59febbe5de63a8c9bd5560ac7344b7
DarioDP <DarioDP@users.noreply.github.com>
2015-04-09 12:40:27 +0200

    Update PanZoomLayer.h

--- Classes/PanZoomLayer.h
+++ Classes/PanZoomLayer.h
@@ -1,62 +1,55 @@
-
-// Created By GrowingDever 21th January 2014
-
-#ifndef _PAN_ZOOM_LAYER_H_
-#define _PAN_ZOOM_LAYER_H_
+#ifndef __PANZOOMLAYER_H__
+#define __PANZOOMLAYER_H__
 
 #include "cocos2d.h"
 
 using namespace cocos2d;
 
-class PanZoomLayer : public CCLayerColor
-{
+class PanZoomLayer : public LayerColor {
+
 private:
-	CCArray *_touches;
-	CCPoint _beganTouchPoint;
-	CCPoint _endedTouchPoint;
-	CCPoint _deltaSum;
-	CCPoint _prevDeltaPoint;
-	double _accelerationFactor;
-	cc_timeval _timeStamp;
-	CCRect _panBoundsRect;
-	float _maxScale;
-	float _minScale;
-	float _productFactor;
-
-	bool _isHolding;
-
+  Vector<Touch *> _touches;
+  Vec2 _beganTouchPoint;
+  Vec2 _endedTouchPoint;
+  Vec2 _deltaSum;
+  Vec2 _prevDeltaPoint;
+  double _accelerationFactor;
+  timeval _timeStamp;
+  Rect _panBoundsRect;
+  float _maxScale;
+  float _minScale;
+  float _productFactor;
+  bool _isHolding;
 
 public:
-	PanZoomLayer();
-	virtual ~PanZoomLayer();
+  PanZoomLayer() {};
+  virtual ~PanZoomLayer() {};
 
-	static PanZoomLayer* create();
+  static PanZoomLayer *create();
 
-	virtual bool init();
-	virtual void onEnter();
-	virtual void onExit();
+  virtual bool init();
+  virtual void onEnter();
+  virtual void onExit();
 
-	virtual void update( float dt );
+  virtual void update(float dt);
 
-	virtual void ccTouchesBegan(CCSet *pTouches, CCEvent *pEvent);
-	virtual void ccTouchesMoved(CCSet *pTouches, CCEvent *pEvent);
-	virtual void ccTouchesEnded(CCSet *pTouches, CCEvent *pEvent);
+  virtual void onTouchesBegan(const std::vector<Touch *> &touches, Event *event);
+  virtual void onTouchesMoved(const std::vector<Touch *> &touches, Event *event);
+  virtual void onTouchesEnded(const std::vector<Touch *> &touches, Event *event);
 
-	virtual void setPosition( CCPoint position );
-	virtual void setScale( float scale );
+  virtual void setPosition(Vec2 position);
+  virtual void setScale(float scale);
 
-	void SetPanBoundsRect( CCRect rect );
-	void SetMaxScale( float maxScale );
-	float GetMaxScale();
-	void SetMinScale( float minScale );
-	float GetMinScale();
+  void SetPanBoundsRect(Rect rect);
+  void SetMaxScale(float maxScale);
+  float GetMaxScale();
+  void SetMinScale(float minScale);
+  float GetMinScale();
 
-	void Holding();
-	void UnHolding();
+  void Holding();
+  void UnHolding();
 
-	void SetProductFactor( float v );
-
+  void SetProductFactor(float v);
 };
 
-
-#endif
+#endif // __PANZOOMLAYER_H__
