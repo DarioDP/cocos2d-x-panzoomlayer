8eb77ac12634df930f374428a7dc477218a9f32f
DarioDP <DarioDP@users.noreply.github.com>
2015-04-09 12:34:53 +0200

    Update HelloWorldScene.cpp

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,96 +1,46 @@
 #include "HelloWorldScene.h"
 #include "PanZoomLayer.h"
 
-USING_NS_CC;
+using namespace cocos2d;
 
-CCScene* HelloWorld::scene()
-{
-    // 'scene' is an autorelease object
-    CCScene *scene = CCScene::create();
-    
-    // 'layer' is an autorelease object
-    HelloWorld *layer = HelloWorld::create();
-
-    // add layer as a child to scene
+Scene* HelloWorld::scene() {
+    auto scene = Scene::create();
+    auto layer = HelloWorld::create();
     scene->addChild(layer);
-
-    // return the scene
     return scene;
 }
 
-// on "init" you need to initialize your instance
-bool HelloWorld::init()
-{
-    //////////////////////////////
-    // 1. super init first
-    if ( !CCLayer::init() )
-    {
+bool HelloWorld::init() {
+    if (!Layer::init()) {
         return false;
     }
     
-    CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
-    CCPoint origin = CCDirector::sharedDirector()->getVisibleOrigin();
+    Size visibleSize = Director::getInstance()->getVisibleSize();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
+    auto map = Sprite::create("Fishfam_base_TH8.jpg");
+    auto mapSize = map->getContentSize();
+    map->setAnchorPoint(Vec2(0, 0));
+    map->setPosition(mapSize * -0.5f);
+    PanZoomLayer *pzLayer = PanZoomLayer::create();
+    this->addChild(pzLayer);
+    pzLayer->SetPanBoundsRect(Rect(mapSize.width * -0.5f, mapSize.height * -0.5f, mapSize.width * 1.0f, mapSize.height * 1.0f));
+    pzLayer->setScale(1.0f);
+    pzLayer->addChild(map);
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
-                                        "CloseNormal.png",
-                                        "CloseSelected.png",
-                                        this,
-                                        menu_selector(HelloWorld::menuCloseCallback));
-    
-	pCloseItem->setPosition(ccp(origin.x + visibleSize.width - pCloseItem->getContentSize().width/2 ,
-                                origin.y + pCloseItem->getContentSize().height/2));
-
-    // create menu, it's an autorelease object
-    CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
-    pMenu->setPosition(CCPointZero);
-    this->addChild(pMenu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-
-	CCSprite *map = CCSprite::create( "Fishfam_base_TH8.jpg" );
-	map->setOpacity( 150 );
-	map->setAnchorPoint( ccp( 0, 0 ) );
-	map->setPosition( map->getContentSize() * -0.5 );
-
-
-	PanZoomLayer *pzLayer = PanZoomLayer::create();
-	this->addChild( pzLayer );
-
-	pzLayer->SetPanBoundsRect( CCRectMake( 
-		map->getContentSize().width * -0.5,
-		map->getContentSize().height * -0.5,
-		map->getContentSize().width * 1.0,
-		map->getContentSize().height * 1.0) );
-	pzLayer->setScale( 2.0 );
-
-	pzLayer->addChild( map, -1 );
-	//map->setPosition( pzLayer->getContentSize() * 0.5 );
-
-	CCSprite *mark = CCSprite::create( "HelloWorld.png" );
-	map->addChild( mark );
-	mark->setPosition( map->getContentSize() * 0.5 );
-    
     return true;
 }
 
 
-void HelloWorld::menuCloseCallback(CCObject* pSender)
-{
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT) || (CC_TARGET_PLATFORM == CC_PLATFORM_WP8)
-	CCMessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
-#else
-    CCDirector::sharedDirector()->end();
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
+void HelloWorld::menuCloseCallback(Ref * pSender) {
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+  MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.", "Alert");
+  return;
 #endif
+
+  Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+  exit(0);
 #endif
 }
+
